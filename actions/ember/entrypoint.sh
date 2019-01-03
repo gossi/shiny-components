23d0ea2320d8adde23913c267d9f895d1c19bdf9
gossi <gossi@users.noreply.github.com>
2019-01-03 18:50:31 +0100

    wrong global :frowning:

--- actions/ember/entrypoint.sh
+++ actions/ember/entrypoint.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-git --global config user.name $GIT_USER
-git --global config user.email $GIT_EMAIL
+git config --global user.name $GIT_USER
+git config --global user.email $GIT_EMAIL
 
 sh -c "yarn ember $*"
